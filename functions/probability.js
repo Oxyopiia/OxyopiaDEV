326fda8aff0445611de7442a2c90e31e33660dd7
Oxyopiia <noahharryb@gmail.com>
2023-01-29 19:38:10 +0000

    Added option to toggle roll data

--- functions/probability.js
+++ functions/probability.js
@@ -1,24 +1,17 @@
 /**
  * Generates a random element out of an array with weights specified
  * @param {[{id: any, weight: integer}]} choices Array of choices objects
+ * @param {boolean} returnData Wether the function should return extra data on the roll
  * @returns {object} Chosen element from the pool of choices
  */
-function selectChoice(choices) {
+function selectChoice(choices, returnData) {
     if (!Array.isArray(choices)) return TypeError("Not an array of choices")
 
     let totalWeight = 0;
 
     for (choice of choices) {
-        if(!choice.id) {
-            return ReferenceError(`Choice does not have id`)
-        }
-
         if(!choice.weight) {
             choice.weight = 1
-        }
-
-        if(choice.weight % 1 != 0) {
-            return TypeError(`${choice.id} weight is not integer (weight is proportional, just multiply all values by 10 or 100, they don't have to add to 100)`)
         }
 
         totalWeight += choice.weight
@@ -30,22 +23,33 @@ function selectChoice(choices) {
     for(choice of choices) {
         let min = counted + 1
         let max = choice.weight + min - 1
-
+        
         if (rand >= min && rand <= max) {
-            return {...choice, roll: {
-                probability: choice.weight / totalWeight ,
-                totalWeight: totalWeight,
-                seed: rand,
-                range: max - min + 1
-            }};
+            if(returnData) {
+                function gcd(a, b) {
+                    if (!b) return a;
+                    
+                    return gcd(b, a % b);
+                };
+                let d = gcd(choice.weight,totalWeight)
+
+
+                return {...choice, roll: {
+                    probability: {
+                        decimal: choice.weight / totalWeight,
+                        fraction: `${choice.weight / d}/${totalWeight / d}`,
+                        expressed: `1 in ${totalWeight / choice.weight}`
+                    },
+                    totalWeight,
+                    rand,
+                    range: max - min + 1
+                }};
+
+            } else return choice
         } 
 
         counted += choice.weight
     }
 }
-
-
-
-
 
 
